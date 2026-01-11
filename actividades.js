e6520c47123fc58c2e9f83001bc436bb9a55e347
Lilen <lilen.chu@hotmail.com>
2026-01-11 13:22:37 -0300

    Enunciados de los ejercicios

---
+++ actividades.js
@@ -0,0 +1,31 @@
+const prompt = require('prompt-sync')();
+
+/*● Ejercicio 1:
+Declara dos variables numéricas numero1 y numero2. Pide al usuario que ingrese dos números y muestra cuál es mayor o si son iguales.*/
+
+/*● Ejercicio 2:
+Defi ne dos constantes, RANGO_MINIMO y RANGO_MAXIMO, con valores numéricos de tu elección. Pide al usuario que ingrese un número y verifica si está dentro del rango definido por las constantes.*/
+
+/*● Ejercicio 3:
+Declara dos variables booleanas condicion1 y condicion2. Pide al usuario que ingrese dos valores booleanos (true o false) y muestra el resultado de diversas combinaciones lógicas.*/
+
+/*● Ejercicio 4:
+Declara una variable nombre y pide al usuario que ingrese su nombre. Verifica si el nombre ingresado es igual a tu nombre.*/
+
+/*● Ejercicio 5:
+Escribe un programa que pida al usuario que ingrese tres números y determine cuál es el mayor de los tres.*/
+
+/*● Ejercicio 6:
+Pide al usuario que ingrese su edad y verifica si es mayor o menor de edad. Muestra un mensaje personalizado según el caso.*/
+
+/*● Ejercicio 7:
+Pide al usuario que ingrese su peso en kilogramos y conviértelo a libras. Muestra el resultado con un mensaje. (Averigua como pasar de kg a libras, Pista: 2.20462)*/
+
+/*● Ejercicio 8:
+Pide al usuario que ingrese las longitudes de los tres lados de un triángulo. Determina y muestra si el triángulo es equilátero, isósceles o escaleno. (Investiga sobre los triángulos para determinar la formula)*/
+
+/*● Ejercicio 9:
+Defi ne una constante PI con el valor de pi (3.14159). Pide al usuario que ingrese el radio de un círculo y calcula su área y perímetro utilizando la constante PI.*/
+
+/*● Ejercicio 10:
+Pide al usuario que ingrese un número del 1 al 7 y muestra el día de la semana correspondiente. Si el número no está dentro de ese rango, muestra un mensaje de error.*/
